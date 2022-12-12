6ff39f0a0081487c980e5632ff8a80340276f968
Diyorbek1070 <dcharshamboyev@gmail.com>
2022-12-12 09:55:49 +0500

    Hjhjef

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,46 +1,30 @@
-const listEl=document.querySelector(".list");
-const mainEl=document.querySelector(".main-flag");
+const mFlag = document.querySelector(".main-flag__flag");
+const mHeading = document.querySelector(".main-flag__heading");
+const list = document.querySelector(".list");
+const mainFlag = document.querySelector(".main-flag");
+const list1 = document.querySelector(".list__flag1")
+const list2 = document.querySelector(".list__flag2")
+const list3 = document.querySelector(".list__flag3")
 
-mainEl.addEventListener('click', ()=>{
-    if(listEl.style.display=="block"){
-        listEl.style.display="none"
-    }
-    else{
-        listEl.style.display="block"
-    }
-})
-
-const flagEl1=document.querySelector(".list__flag1");
-const flagEl2=document.querySelector(".list__flag2");
-const flagEl3=document.querySelector(".list__flag3");
-
-flagEl1.addEventListener("click",()=>{
-    document.querySelector(".block").style.display="block";
-    document.querySelector(".heading-block").style.display="block";
-    document.querySelector(".main-flag__flag").style.display="none";
-    document.querySelector(".main-flag__heading").style.display="none";
-    document.querySelector(".block1").style.display="none";
-    document.querySelector(".heading-block1").style="none";
-})
-
-flagEl2.addEventListener("click",()=>{
-    document.querySelector(".main-flag__flag").style.display="block";
-    document.querySelector(".main-flag__heading").style.display="block";
-    document.querySelector(".block").style.display="none";
-    document.querySelector(".heading-block").style.display="none";
-    document.querySelector(".block1").style.display="none";
-    document.querySelector(".heading-block1").style="none";
-})
-
-flagEl3.addEventListener("click",()=>{
-    document.querySelector(".block1").style.display="block";
-    document.querySelector(".heading-block1").style.display="block";
-    document.querySelector(".main-flag__flag").style.display="none";
-    document.querySelector(".main-flag__heading").style.display="none";
-    document.querySelector(".block").style.display="none";
-    document.querySelector(".heading-block").style.display="none";
+mainFlag.addEventListener("click", () => {
+    list.style.display = "block"
 })
 
 
+list1.addEventListener('click', () => {
+    mFlag.src = "./images/Rusian.png";
+    mHeading.innerHTML = "Russian";
+    list.style.display = "none"
+})
 
+list2.addEventListener('click', () => {
+    mFlag.src = "./images/UZB.jfif";
+    mHeading.innerHTML = "Uzbek";
+    list.style.display = "none"
+})
 
+list3.addEventListener('click', () => {
+    mFlag.src = "./images/english.webp";
+    mHeading.innerHTML = "English";
+    list.style.display = "none"
+})
